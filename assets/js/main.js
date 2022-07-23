6fadecb17c48315712534a11d65734330df91448
ahargunyllib <billy.bpm03@gmail.com>
2022-07-23 23:31:59 +0700

    add home animation, navbar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         })
     })
 })
+
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header')
+
+  header.classList.toggle('nav-link-scrolling', window.scrollY > 800)
+})
+
+const text = document.querySelector(".wavy-text")
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>")
+const element = document.querySelectorAll('span')
+for (let i=0; i<element.length; i++) {
+  element[i].style.animationDelay = i*0.05 + 's';
+}
+
+const section = document.querySelectorAll("section")
+const link = document.querySelectorAll(".nav-link")
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  link.forEach(ltx => ltx.classList.remove("nav-link-active"))
+  link[len].classList.add("nav-link-active")
+}
+activeMenu()
+window.addEventListener("scroll", activeMenu)
